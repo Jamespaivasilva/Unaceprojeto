4d51cac0cdccd3414cb55d80d51281133304f5ee
Jamespaivasilva <42623396+Jamespaivasilva@users.noreply.github.com>
2019-06-19 19:02:48 -0300

    Update gerenciarempresas.php

--- interface/gerenciarempresas.php
+++ interface/gerenciarempresas.php
@@ -1,6 +1,5 @@
 <?php
-   //ignorar erros e alertas 
-   error_reporting(0);
+   
    //impedir que quem não acessou tenha acesso ao conteúdo da página!
    require_once("../classes/validar.php");
    
@@ -230,6 +229,6 @@
    <script src="../scripts/parsley.js"></script>
    <script src="../scripts/gerenciarempresa.js"></script>
 </body>
-<br/><br/><br/><br/><br/>
+
 <?php require_once("rodape.php");?>
 </html>
