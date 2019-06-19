aafbc1738d2f9eae5994b5bcc604d704bdbb033f
Jamespaivasilva <42623396+Jamespaivasilva@users.noreply.github.com>
2019-06-19 18:59:52 -0300

    Update disponibilidade.php

--- interface/disponibilidade.php
+++ interface/disponibilidade.php
@@ -1,6 +1,5 @@
 <?php
-   //ignorar erros e alertas
-   error_reporting(0);
+   /
    //impedir que quem não acessou tenha acesso ao conteúdo da página!
    require_once("../classes/validar.php");
    
@@ -118,6 +117,6 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
-<br/><br/><br/>
+
 <?php require_once("rodape.php");?>
 </html>
